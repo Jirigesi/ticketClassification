55a6503f1ea4c975aeb7a56a37aeb903a28731f0
Jirigesi <50706568+Jirigesi@users.noreply.github.com>
2021-11-16 14:35:58 -0800

    change

--- bert1/main.py
+++ bert1/main.py
@@ -10,7 +10,7 @@ from utils import data_preprocess
 if __name__ == '__main__':
     file_path = "../../data.csv"
     data = pd.read_csv(file_path)
-    train_df, val_df, test_target = data_preprocess(data)
+    train_df, val_df = data_preprocess(data)
 
     BERT_MODEL_NAME = "bert-base-cased"
     tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
--- bert1/utils.py
+++ bert1/utils.py
@@ -2,62 +2,64 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import re
 from data_clean import clean_data
-# def read_data(file_path, RANDOM_SEED=42):
-#     df = pd.read_csv(file_path)
-#     df = clean(df, )
+
+
+def data_preprocess(file_path, RANDOM_SEED=42):
+    df = pd.read_csv(file_path)
+    df = clean_data(df, ['summary', 'description'])
+
+    train_df, val_df = train_test_split(df, test_size=0.2, random_state=RANDOM_SEED)
+    train_df.shape, val_df.shape
+
+    not_bug_train_df = train_df[train_df['label'] == 1]
+    bug_train_df = train_df[train_df['label'] == 0]
+    not_bug_train_df.shape, bug_train_df.shape
+
+    train_df = pd.concat([
+        not_bug_train_df,
+        bug_train_df.sample(6400)
+    ])
+    train_df = train_df.sample(frac=1)
+    val_df = val_df.sample(frac=1)
+
+    train_df = train_df.reset_index(drop=True)
+    val_df = val_df.reset_index(drop=True)
+
+    return train_df, val_df
+
+
+
+# def data_preprocess(data):
+#     summary_length = data['summary'].apply(lambda x: len(str(x).split(" ")))
+#     data["summary_token_length"] = summary_length
+#     description_length = data['description'].apply(lambda x: len(str(x).split(" ")))
+#     data["description_token_length"] = description_length
 #
-#     train_df, val_df = train_test_split(df, test_size=0.2, random_state=RANDOM_SEED)
-#     train_df.shape, val_df.shape
+#     data = clean_data(data, ['summary', 'description'])
 #
-#     not_bug_train_df = train_df[train_df['label'] == 1]
-#     bug_train_df = train_df[train_df['label'] == 0]
-#     not_bug_train_df.shape, bug_train_df.shape
+#     data['combined'] = data['summary'] + ' ' + data['description']
 #
-#     train_df = pd.concat([
-#         not_bug_train_df,
-#         bug_train_df.sample(6400)
-#     ])
-#     train_df = train_df.sample(frac=1)
-#     val_df = val_df.sample(frac=1)
+#     positives = data[data['label'] == 1.0]
+#     negatives = data[data['label'] == 0.0]
 #
-#     train_df = train_df.reset_index(drop=True)
-#     val_df = val_df.reset_index(drop=True)
+#     positives_train, positives_test = train_test_split(positives, test_size=0.2)
+#     positive_train_nums = positives_train.shape[0]
 #
-#     return train_df, val_df
-
-
-
-def data_preprocess(data):
-    summary_length = data['summary'].apply(lambda x: len(str(x).split(" ")))
-    data["summary_token_length"] = summary_length
-    description_length = data['description'].apply(lambda x: len(str(x).split(" ")))
-    data["description_token_length"] = description_length
-
-    data = clean_data(data, ['summary', 'description'])
-
-    data['combined'] = data['summary'] + ' ' + data['description']
-
-    positives = data[data['label'] == 1.0]
-    negatives = data[data['label'] == 0.0]
-
-    positives_train, positives_test = train_test_split(positives, test_size=0.2)
-    positive_train_nums = positives_train.shape[0]
-
-    # get 1: 1 negative training data
-    negative_total_nums = data[data['label'] == 0.0].shape[0]
-    negative_test_size = 1 - positive_train_nums / negative_total_nums
-    negatives_train, negatives_test = train_test_split(negatives, test_size=negative_test_size)
-
-    train = pd.concat([positives_train, negatives_train]).sample(frac=1).reset_index(drop=True)
-    test = pd.concat([positives_test, negatives_test]).sample(frac=1).reset_index(drop=True)
-
-    # cadidates: summary, description, combined
-    SELECT_TEXT = "combined"
-    train = train[[SELECT_TEXT, "label"]]
-    test_target = test['label']
-    test = test[[SELECT_TEXT]]
-    train = train.rename(columns={SELECT_TEXT: "text", "label": "target"})
-    test = test.rename(columns={SELECT_TEXT: "text"})
-
-    return train, test, test_target
+#     # get 1: 1 negative training data
+#     negative_total_nums = data[data['label'] == 0.0].shape[0]
+#     negative_test_size = 1 - positive_train_nums / negative_total_nums
+#     negatives_train, negatives_test = train_test_split(negatives, test_size=negative_test_size)
+#
+#     train = pd.concat([positives_train, negatives_train]).sample(frac=1).reset_index(drop=True)
+#     test = pd.concat([positives_test, negatives_test]).sample(frac=1).reset_index(drop=True)
+#
+#     # cadidates: summary, description, combined
+#     SELECT_TEXT = "combined"
+#     train = train[[SELECT_TEXT, "label"]]
+#     test_target = test['label']
+#     test = test[[SELECT_TEXT]]
+#     train = train.rename(columns={SELECT_TEXT: "text", "label": "target"})
+#     test = test.rename(columns={SELECT_TEXT: "text"})
+#
+#     return train, test, test_target
 
